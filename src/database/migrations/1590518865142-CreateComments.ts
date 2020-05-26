84a4ab495fb97b71414ff3c8f8d89c16effa0c83
Gabriel Asorey <gasorey@gmail.com>
2020-05-26 16:05:32 -0300

    sinergia test on going

---
+++ src/database/migrations/1590518865142-CreateComments.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateComments1590518865142 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
